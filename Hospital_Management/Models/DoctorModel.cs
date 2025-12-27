a7b0f91ee00a407667e1379183a610588756fa80
Tree <bhavyasinghal0208@gmail.com>
2025-12-27 23:57:46 +0530

    doctor add,edit, working

---
+++ Hospital_Management/Models/DoctorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Hospital_Management.Models
+{
+    public class DoctorModel
+    {
+        
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public string Specialization { get; set; }
+        public string WorkPlace { get; set; }
+        public int Experience { get; set; }
+        
+
+    }
+}
