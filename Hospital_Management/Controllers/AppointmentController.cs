f99b5c67bc6d84b3a79482e906815f52c5f389a8
LENOVO <LENOVO@TILLU>
2025-12-28 20:10:25 +0530

    appointment crud

--- Hospital_Management/Controllers/AppointmentController.cs
+++ Hospital_Management/Controllers/AppointmentController.cs
@@ -1,78 +1,136 @@
-﻿//using Hospital_Management.Models;
-//using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Hospital_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
-//public class AppointmentController : Controller
-//{
-//    string con = ConfigurationManager.ConnectionStrings["dbcon"].ToString();
+namespace Hospital_Management.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly string _con;
 
-//    public ActionResult Index()
-//    {
-//        using (SqlConnection db = new SqlConnection(con))
-//        {
-//            string q = @"select A.AppointmentId,D.DoctorName,
-//                         P.PatientName,A.AppointmentDate,A.AppointmentTime
-//                         from Appointments A
-//                         join Doctors D on A.DoctorId=D.DoctorId
-//                         join Patients P on A.PatientId=P.PatientId";
-//            return View(db.Query<AppointmentModel>(q).ToList());
-//        }
-//    }
+        public AppointmentController(IConfiguration configuration)
+        {
+            _con = configuration.GetConnectionString("dbcon");
+        }
 
-//    public ActionResult Create()
-//    {
-//        using (SqlConnection db = new SqlConnection(con))
-//        {
-//            ViewBag.Doctors = db.Query<DoctorModel>("select * from Doctors");
-//            ViewBag.Patients = db.Query<PatientModel>("select * from Patients");
-//        }
-//        return View();
-//    }
+        // INDEX: List all appointments
+        public IActionResult Index()
+        {
+            using (var db = new SqlConnection(_con))
+            {
+                var appointments = db.Query<AppointmentModel>(@"
+                   SELECT A.AppointmentId,
+                          D.DoctorName,
+                          P.PatientName,
+                          A.AppointmentDate,
+                          A.AppointmentTime
+                   FROM Appointments A
+                   JOIN Doctors D ON A.DoctorId = D.DoctorId
+                   JOIN Patients P ON A.PatientId = P.PatientId
+                ").ToList();
 
-//    [HttpPost]
-//    public ActionResult Create(AppointmentModel m)
-//    {
-//        using (SqlConnection db = new SqlConnection(con))
-//        {
-//            db.Execute(@"insert into Appointments
-//            values(@DoctorId,@PatientId,@AppointmentDate,@AppointmentTime)", m);
-//        }
-//        return RedirectToAction("Index");
-//    }
+                return View(appointments);
+            }
+        }
 
-//    public ActionResult Edit(int id)
-//    {
-//        using (SqlConnection db = new SqlConnection(con))
-//        {
-//            ViewBag.Doctors = db.Query<DoctorModel>("select * from Doctors");
-//            ViewBag.Patients = db.Query<PatientModel>("select * from Patients");
+        // CREATE: Show form
+        public IActionResult Create()
+        {
+            using (var db = new SqlConnection(_con))
+            {
+                ViewBag.Doctors = db.Query<DoctorModel>("SELECT * FROM Doctors").ToList();
+                ViewBag.Patients = db.Query<PatientModel>("SELECT * FROM Patients").ToList();
+            }
 
-//            return View(db.QueryFirstOrDefault<AppointmentModel>(
-//             "select * from Appointments where AppointmentId=@id", new { id }));
-//        }
-//    }
+            return View();
+        }
 
-//    [HttpPost]
-//    public ActionResult Edit(AppointmentModel m)
-//    {
-//        using (SqlConnection db = new SqlConnection(con))
-//        {
-//            db.Execute(@"update Appointments set
-//   DoctorId=@DoctorId,
-//   PatientId=@PatientId,
-//   AppointmentDate=@AppointmentDate,
-//   AppointmentTime=@AppointmentTime
-//   where AppointmentId=@AppointmentId", m);
-//        }
-//        return RedirectToAction("Index");
-//    }
+        // CREATE: Save form
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(AppointmentModel m)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new SqlConnection(_con))
+                {
+                    ViewBag.Doctors = db.Query<DoctorModel>("SELECT * FROM Doctors").ToList();
+                    ViewBag.Patients = db.Query<PatientModel>("SELECT * FROM Patients").ToList();
+                }
+                return View(m);
+            }
 
-//    public ActionResult Delete(int id)
-//    {
-//        using (SqlConnection db = new SqlConnection(con))
-//        {
-//            db.Execute("delete from Appointments where AppointmentId=@id", new { id });
-//        }
-//        return RedirectToAction("Index");
-//    }
+            using (var db = new SqlConnection(_con))
+            {
+                db.Execute(@"
+                   INSERT INTO Appointments
+                   (DoctorId, PatientId, AppointmentDate, AppointmentTime)
+                   VALUES (@DoctorId, @PatientId, @AppointmentDate, @AppointmentTime)
+                ", m);
+            }
 
-//}
+            return RedirectToAction(nameof(Index));
+        }
+
+        // EDIT: Show edit form
+        public IActionResult Edit(int id)
+{
+    using var db = new SqlConnection(_con);
+
+    ViewBag.Doctors = db.Query<DoctorModel>("SELECT * FROM Doctors").ToList();
+    ViewBag.Patients = db.Query<PatientModel>("SELECT * FROM Patients").ToList();
+
+    var appointment = db.QueryFirstOrDefault<AppointmentModel>(
+        "SELECT * FROM Appointments WHERE AppointmentId = @id",
+        new { id });
+
+    if (appointment == null)
+        return NotFound();
+
+    return View(appointment);
+}
+
+
+        // EDIT: Save changes
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(AppointmentModel m)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new SqlConnection(_con))
+                {
+                    ViewBag.Doctors = db.Query<DoctorModel>("SELECT * FROM Doctors").ToList();
+                    ViewBag.Patients = db.Query<PatientModel>("SELECT * FROM Patients").ToList();
+                }
+                return View(m);
+            }
+
+            using (var db = new SqlConnection(_con))
+            {
+                db.Execute(@"
+                   UPDATE Appointments SET
+                   DoctorId = @DoctorId,
+                   PatientId = @PatientId,
+                   AppointmentDate = @AppointmentDate,
+                   AppointmentTime = @AppointmentTime
+                   WHERE AppointmentId = @AppointmentId
+                ", m);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // DELETE: Delete appointment
+        public IActionResult Delete(int id)
+        {
+            using (var db = new SqlConnection(_con))
+            {
+                db.Execute("DELETE FROM Appointments WHERE AppointmentId = @id", new { id });
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Hospital_Management/Models/AppointmentModel.cs
+++ Hospital_Management/Models/AppointmentModel.cs
@@ -1,15 +1,28 @@
-﻿namespace Hospital_Management.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hospital_Management.Models
 {
     public class AppointmentModel
     {
         public int AppointmentId { get; set; }
+
+        [Required]
         public int DoctorId { get; set; }
+
+        [Required]
         public int PatientId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime AppointmentDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
         public TimeSpan AppointmentTime { get; set; }
 
+        // For Index (JOIN result)
         public string DoctorName { get; set; }
         public string PatientName { get; set; }
     }
-
 }
